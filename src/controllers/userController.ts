4298a1b9478fb93c01a953c9673313d61ef405f7
parathantl <parathanlive123@gmail.com>
2025-02-07 01:22:26 +0530

    fix: allow cors url

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -67,8 +67,8 @@ export async function loginUser(req: Request, res: Response) {
     // Set token in HTTP-only cookie for better security
     res.cookie('auth_token', token, {
         httpOnly: true,
-        secure: true,        // False for local development (HTTP)
-        sameSite: 'none',      // 'lax' is safer and works over HTTP
+        secure: false,        // False for local development (HTTP)
+        sameSite: 'lax',      // 'lax' is safer and works over HTTP
         maxAge: 2 * 24 * 60 * 60 * 1000
     });      
     
